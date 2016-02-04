92f5beb972c1d85a3aecabf8576a13de48c2e60a
Jesus Flores <joy_warmgun@hotmail.com>
2016-02-04 17:56:49 -0600

    Probando el tema cursos para vista alumno

--- Temas/tema2015cursos/layout/contenido.php
+++ Temas/tema2015cursos/layout/contenido.php
@@ -1,55 +1,32 @@
-<!--Contenido de la pagina-->
-    <div id="page-content">
-    	<div id="region-main-box">
-       		<div id="region-post-box">
-                <div id="region-main-wrap">
-                    <?php if ( ($hassidepost OR (right_to_left() AND $hassidepre)) and ($hassidepre OR (right_to_left() AND $hassidepost)) ) { ?>
-                        <div id="region-main">
-                        <div class="region-content">
-                    <?php }else if ( !($hassidepost OR (right_to_left() AND $hassidepre)) and ($hassidepre OR (right_to_left() AND $hassidepost)) ){?>
-                        <div id="region-main" style="width:100%">
-                        <div class="region-content" style="width:78%">
-                    <?php }else{?>
-                   	<?php if(isset($PAGE->activityrecord->content)) { ?>
-                   		<div id="region-main" style="width:115%;margin-left:180px;margin-top:-25px;overflow:visible;">
-                  	<?php }else{?>
-                     	<div id="region-main" style="width:100%;margin-left:190px;overflow:visible;">
-                  	<?php }?>
-                        <div class="region-content" style="width:100%">
-                    <?php }?>
-                  	<?php echo $coursecontentheader; ?>
-                  	<?php echo $OUTPUT->main_content() ?>
-              		<?php echo $coursecontentfooter; ?>
-               	</div>
-          	</div>
-      	</div>
-
-     	<?php if ($hassidepre OR (right_to_left() AND $hassidepost)) { ?>
-  		<div id="region-pre" class="block-region">
-        	<div class="region-content">
-             	<?php
-                	if (!right_to_left()) {
-                   	echo $OUTPUT->blocks_for_region('side-pre');
-                   	} elseif ($hassidepost) {
-                  	echo $OUTPUT->blocks_for_region('side-post');
-                    } ?>
-         	</div>
-    	</div>
-         	
-		<?php } ?>
-        <?php if ($hassidepost OR (right_to_left() AND $hassidepre)) { ?>
-      	<div id="region-post" class="block-region">
-        	<div class="region-content">
-            	<?php
-               	if (!right_to_left()) {
-          		echo $OUTPUT->blocks_for_region('side-post');
-             	} elseif ($hassidepre) {
-           		echo $OUTPUT->blocks_for_region('side-pre');
-              	} ?>
-          	</div>
-    	</div>
-        <?php } ?>
-   	</div>
-    </div></div>
-    
-<!--FIN Contenido de la pagina-->
+<div id="page-content">
+    <div id="region-main-box">
+        <div id="region-post-box">
+            <?php if ( is_siteadmin() ) { ?>
+            <div id="region-main-wrap">
+            <?php }else{ ?>
+            <div id="region-main-wrap" style="right:90%;width:70%;">  <!-- right: 90%, width: 70%  -->
+            <?php } ?>
+                <div id="region-main">
+                    <div class="region-content">
+                        <?php echo core_renderer::MAIN_CONTENT_TOKEN ?>
+                    </div>
+                </div>
+            </div>
+            <?php if ( $hassidepre AND is_siteadmin() ) { ?>
+                <div id="region-pre">
+                    <div class="region-content">
+                        <?php echo $OUTPUT->blocks_for_region('side-pre') ?>
+                    </div>
+                </div>
+                <?php } ?>
+ 
+                <?php if ( $hassidepost AND is_siteadmin() ) { ?>
+                <div id="region-post">
+                    <div class="region-content">
+                        <?php echo $OUTPUT->blocks_for_region('side-post') ?>
+                    </div>
+                </div>
+            <?php } ?>
+        </div>
+    </div>
+</div>
